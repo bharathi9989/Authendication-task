fd2376310db0c2d43b75def36c21dadf3ceee69f
bharathi9989 <er.velubharathi.s@gmail.com>
2026-04-17 15:12:56 +0530

    feat : added a get me file and error handling

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -12,6 +12,9 @@ export const register = async (req, res) => {
     const existing = await User.findOne({ email });
     if (existing)
       return res.status(400).json({ message: "User already exists" });
+    if (password.length < 6) {
+      return res.status(400).json({ message: "Password too short" });
+    }
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
