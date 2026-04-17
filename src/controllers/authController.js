8b6c806345107e31c4b259d5588c42452fe676d1
bharathi9989 <er.velubharathi.s@gmail.com>
2026-04-17 13:33:56 +0530

    feat : added a login logic in controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -29,3 +29,25 @@ export const register = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = generateToken(user);
+
+    res.json({
+      token,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
