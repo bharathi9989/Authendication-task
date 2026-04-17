8fccd82076e513c3845daea3094f20d9ae493086
bharathi9989 <er.velubharathi.s@gmail.com>
2026-04-17 14:32:24 +0530

    refact : added a new feature in login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -23,7 +23,7 @@ export const register = async (req, res) => {
     });
 
     res.status(201).json({
-        message: "User registered successfully",
+      message: "User registered successfully",
     });
   } catch (err) {
     res.status(500).json({ message: err.message });
@@ -46,6 +46,11 @@ export const login = async (req, res) => {
 
     res.json({
       token,
+      user: {
+        id: user._id,
+        email: user.email,
+        username: user.username,
+      },
     });
   } catch (err) {
     res.status(500).json({ message: err.message });
