07045f204adc3bbaf3b6fc1e4496273e38597a7e
bharathi9989 <er.velubharathi.s@gmail.com>
2026-04-17 13:33:17 +0530

    feat : updated a controller with logic

---
+++ src/controllers/authController.js
@@ -0,0 +1,31 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../utils/generateToken.js";
+
+export const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password)
+      return res.status(400).json({ message: "All fields required" });
+
+    const existing = await User.findOne({ email });
+    if (existing)
+      return res.status(400).json({ message: "User already exists" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
